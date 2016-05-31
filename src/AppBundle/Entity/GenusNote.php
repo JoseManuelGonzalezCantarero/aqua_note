dede4e5efe8890a2dfd13ecb11d222022e3f509a
Manu <jgonzalezcantarero@gmail.com>
2016-05-31 09:33:30 +0200

    Saving a Relationship

--- src/AppBundle/Entity/GenusNote.php
+++ src/AppBundle/Entity/GenusNote.php
@@ -113,5 +113,21 @@ class GenusNote
     {
         return $this->id;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getGenus()
+    {
+        return $this->genus;
+    }
+
+    /**
+     * @param mixed $genus
+     */
+    public function setGenus( Genus $genus)
+    {
+        $this->genus = $genus;
+    }
     
 }
