ff218e73a0f4678c54d233927633c8d14478e254
Manu <jgonzalezcantarero@gmail.com>
2016-05-31 11:32:15 +0200

    Querying on a Relationship

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -78,10 +78,7 @@ class GenusController extends Controller
         }
         */
 
-        $recentNotes = $genus->getNotes()
-            ->filter(function (GenusNote $note){
-                return $note->getCreatedAt() > new \DateTime('-3 months');
-            });
+        $recentNotes = $em->getRepository('AppBundle:GenusNote')->findAllRecentNotesForGenus($genus);
         return $this->render('genus/show.html.twig', [
            'genus' => $genus,
             'recentNoteCount' => count($recentNotes)
