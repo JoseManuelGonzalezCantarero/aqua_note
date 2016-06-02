49520caadd8e68ea8c4deb007097318e1711261b
Manu <jgonzalezcantarero@gmail.com>
2016-06-02 11:07:08 +0200

    Render that Form Pretty (Bootstrap)

--- src/AppBundle/Controller/GenusAdminController.php
+++ src/AppBundle/Controller/GenusAdminController.php
@@ -2,16 +2,21 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\GenusFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class GenusAdminController extends Controller
 {
     /**
-     * @Route("/genus/new")
+     * @Route("/genus/new", name="admin_genus_new")
      */
     public function newAction()
     {
+        $form = $this->createForm(GenusFormType::class);
         
+        return $this->render('admin/genus/new.html.twig', [
+            'genusForm' => $form->createView()
+        ]);
     }
 }
